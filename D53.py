7763c9364679c23abda065eec4b3702b7971f6aa
George Kovar <kovargeorge@gmail.com>
2020-12-11 09:31:31 +0100

    D53 finished + cosmetic changes

--- D53.py
+++ D53.py
@@ -4,70 +4,81 @@ from D52 import R_lst, t_lst, L_lst
 from mat import A2195_T84 as mat
 
 
-#cross-sectional enclosed area
+# cross-sectional enclosed area
 def get_area(r):
     A = math.pi * (r ** 2)
     return A
 
-#area moment of inertia
+
+# area moment of inertia
 def get_I(r, t):
     I = math.pi * (r ** 3) * t
     return I
 
-#mass of the structure, cylinder + spherical end caps
+
+# mass of the structure, cylinder + spherical end caps
 def get_mass(r, t, L):
     m = (2 * math.pi * r * t * L + 4 * math.pi * t * (r ** 2)) * rho
     return m
 
-#column buckling critical stress
+
+# column buckling critical stress
 def get_sigma_cr_column(r, t, L):
     sigma_cr_1 = (E_mat * get_I(r, t) * ((math.pi) ** 2)) / (get_area(r) * (L ** 2))
     return sigma_cr_1
 
-#shell buckling critical stress
+
+# shell buckling critical stress
 def get_sigma_cr_shell(r, t):
-    Q = (p / E_mat) * (r / t) ** 2 #dimensionless variable for intermediate step
-    sigma_cr_2 = (1.983 - 0.983 * (math.exp(-23.14 * Q))) * ((E_mat * t) / (r * math.sqrt(1 - v ** 2) * math.sqrt(3)))
+    Q = (p / E_mat) * (r / t) ** 2  # dimensionless variable for intermediate step
+    sigma_cr_2 = (1.983 - 0.983 * (math.exp(-23.14 * Q))) * \
+                 ((E_mat * t) / (r * math.sqrt(1 - v ** 2) * math.sqrt(3)))
     return sigma_cr_2
 
-#stress imposed by launch loads (compressive load at two ends)
+
+# stress imposed by launch loads (compressive load at two ends)
 def get_sigma_cr_applied(r, t, L):
     f = 9.81 * 7.5 * 3.2175 * (get_mass(r, t, L) + m_fuel)  # WP4 2.2.2
-    sigma_applied = f / (2 * math.pi * r * t)               #normal stress
+    sigma_applied = f / (2 * math.pi * r * t)               # normal stress
     return sigma_applied
 
-#check if configuration fails
+
+# check if configuration fails
 def check_config(r, t, L):
     check = True
-    if get_sigma_cr_applied(r, t, L) > get_sigma_cr_column(r, t,L) or get_sigma_cr_applied(r, t, L) > get_sigma_cr_shell(r, t):
+    if get_sigma_cr_applied(r, t, L) > get_sigma_cr_column(r, t,L) or \
+            get_sigma_cr_applied(r, t, L) > get_sigma_cr_shell(r, t)\
+            or get_sigma_cr_applied(r, t, L) > mat.get("sigma_y")*1e6:
         check = False
     return check
 
-#find the optimum config (lowest mass)
+
+# find the optimum config (lowest mass)
 def get_opt(config):
-    masslist = config[3]                #masses
-    opt_mass = min(masslist)            #lowest mass
-    opt = masslist.index(opt_mass)      #index with lowest mass
-    opt_config = [config[0][opt], config[1][opt], config[2][opt], config[3][opt]] #radius thickness length and mass
+    masslist = config[3]                # masses
+    opt_mass = min(masslist)            # lowest mass
+    opt = masslist.index(opt_mass)      # index with lowest mass
+    opt_config = [config[0][opt], config[1][opt], config[2][opt], config[3][opt]]
+    # radius thickness length and mass
     return opt_config
 
-#filter list of configurations
+
+# filter list of configurations
 def buckling_opt(mass_fuel, R_lst, t_lst, L_lst, mat):
     global E_mat, rho, v, p, m_fuel
-    m_fuel = mass_fuel                  #total mass of the propellant
-    E_mat = mat.get("E") * 10 ** 9      #materials young's modulus
+    m_fuel = mass_fuel                  # total mass of the propellant
+    E_mat = mat.get("E") * 10 ** 9      # materials young's modulus
 
-
-    rho = mat.get("rho")    #kg/m^3
-    v = mat.get("nu")       #poission ratio
+    rho = mat.get("rho")    # kg/m^3
+    v = mat.get("nu")       # poission ratio
 
     p = 2.4 * (10 ** 6) * 1.25  # Pa inside pressure, 1.25 margin
 
-    #new lists for viable configs
-    R_viable = []   #radii
-    t_viable = []   #thicknesses
-    L_viable = []   #lengths
-    m_viable = []   #masses
+    # new lists for viable configs
+    R_viable = []   # radii
+    t_viable = []   # thicknesses
+    L_viable = []   # lengths
+    m_viable = []   # masses
 
     for i in range(0, len(R_lst)):
         r = R_lst[i]
@@ -79,10 +90,11 @@ def buckling_opt(mass_fuel, R_lst, t_lst, L_lst, mat):
             L_viable.append(L)
             m_viable.append(get_mass(r, t, L))
 
-    config = [R_viable, t_viable, L_viable, m_viable]   #all viable configurations
-    config_opt = get_opt(config)                        #optimal configuration
-    filtered = len(R_lst) - len(R_viable)               #number of failing configs
+    config = [R_viable, t_viable, L_viable, m_viable]  # all viable configurations
+    config_opt = get_opt(config)                       # optimal configuration
+    filtered = len(R_lst) - len(R_viable)              # number of failing configs
     return [config, config_opt, filtered]
+
 
 '''
 the returned list
